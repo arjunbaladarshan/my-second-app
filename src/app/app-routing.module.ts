b7d1068bced3e084a922123198542a5498aa573e
Arjun Bala <er.arjunbala@gmail.com>
2024-03-14 08:08:38 +0530

    lecture 13-03-2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { DetailStudentComponent } from './detail-student/detail-student.component';
+import { FacultyComponent } from './faculty/faculty.component';
 import { StudentsComponent } from './students/students.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent},
   {path:'students',component:StudentsComponent},
-  {path:'students/:id',component:DetailStudentComponent}
+  {path:'students/:id',component:DetailStudentComponent},
+  {path:'faculties',component:FacultyComponent}
 ];
 
 @NgModule({
--- src/app/faculty/faculty.component.ts
+++ src/app/faculty/faculty.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class FacultyComponent {
 
+  tempFac = {
+    id:'',
+    FacultyName:'',
+    FacultyInitial:''
+  };
+
+  idToUpdate = -1;
+
+  faculites = [
+    {id:'1',FacultyName:'arjun',FacultyInitial:'CEAVB'},
+    {id:'2',FacultyName:'abcd',FacultyInitial:'CEABC'},
+    {id:'3',FacultyName:'def',FacultyInitial:'CEDEF'},
+    {id:'4',FacultyName:'ghi',FacultyInitial:'CEGHI'},
+  ];
+
+  addFaculty(){
+    this.faculites.push({...this.tempFac});
+    this.tempFac.id = '';
+    this.tempFac.FacultyName = '';
+    this.tempFac.FacultyInitial = '';
+  }
+
+  editFaculty(){
+    for(let i=0;i<this.faculites.length;i++){
+      if(Number(this.faculites[i].id)==this.idToUpdate){
+        this.faculites[i] = {...this.tempFac};
+      }
+    }
+    this.idToUpdate = -1;
+    this.tempFac.id = '';
+    this.tempFac.FacultyName = '';
+    this.tempFac.FacultyInitial = '';
+  }
+
+  setDataFaculty(id:any){
+    this.idToUpdate = id;
+    for(let i=0;i<this.faculites.length;i++){
+      if(Number(this.faculites[i].id)==this.idToUpdate){
+        this.tempFac = {...this.faculites[i]};
+      }
+    }
+  }
+
+  deleteFaculty(id:any){
+    let indexToDelete = -1; 
+    for(let i=0;i<this.faculites.length;i++){
+      if(Number(this.faculites[i].id)==id){
+        indexToDelete = i;
+      }
+    }
+    this.faculites.splice(indexToDelete,1);
+  }
 }
